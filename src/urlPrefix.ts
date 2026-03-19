07ec482768e8e957b220c1fbff0c72374ba25a09
FriquetLuca <friquet.luca@gmail.com>
2026-03-19 15:35:42 +0100

    fixing the url prefix for external datas

--- src/urlPrefix.ts
+++ src/urlPrefix.ts
@@ -1,1 +1,10 @@
-export const urlPrefix = (src: string) => `${import.meta.env.BASE_URL}${src}`;
+export const urlPrefix = (src: string) => {
+  if (
+    /^(?:[a-z+]+:)?\/\//i.test(src) ||
+    src.startsWith('data:') ||
+    src.startsWith('blob:')
+  ) {
+    return src;
+  }
+  return `${import.meta.env.BASE_URL}${src.replace(/^\//, '')}`;
+};
