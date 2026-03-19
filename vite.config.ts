a129b514aef96cd9e0e3ceeb61729ed74daff741
FriquetLuca <friquet.luca@gmail.com>
2026-03-19 20:35:36 +0100

    add vitest for unit testing and an example

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -14,6 +15,11 @@ export default defineConfig(({ command }) => {
       }),
       tailwindcss(),
     ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+    },
     server: {
       cors: true, // enable CORS for all origins
     },
